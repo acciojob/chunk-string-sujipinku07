37f62c51776fe4a33cb3edbfc6e3c923c23c17bb
sujipinku07 <sujipinku07@gmail.com>
2024-09-30 19:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	if(!str || size<=0){
+		return []
+	}
+	const chunks=[];
+	for(let i=0;i<str.length;i+=size){
+		chunks.push(str.slice(i,i+size))
+	}
+	return chunks;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+ const str = prompt("Enter String.");
+ const size = prompt("Enter Chunk Size.");
+alert(stringChop(str, size));
